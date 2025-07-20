53e1bd6d0f21fe98cb6772c82b78f5d33d29ec89
SoullessMindful <soullessmindful@gmail.com>
2025-07-20 20:47:18 +0200

    generate_data: a basic data for training generation script created.

---
+++ generate_data.py
@@ -0,0 +1,30 @@
+from functools import reduce
+import json
+import random
+import sys
+from typing import Final
+
+
+VARIABLE_COUNT: Final = 10
+INPUTS_COUNT: Final = 10000
+
+
+def generate_data():
+    inputs: Final = [
+        [random.uniform(-2, 2) for _ in range(VARIABLE_COUNT)]
+        for _ in range(INPUTS_COUNT)
+    ]
+    outputs: Final = [
+        [reduce(lambda product, element: product * element, input_row, 1.0)]
+        for input_row in inputs
+    ]
+    return inputs, outputs
+
+
+if __name__ == "__main__":
+    inputs, outputs = generate_data()
+
+    path: Final = './data/data.json' if (len(sys.argv) <= 1) else sys.argv[1]
+
+    with open(path, "w", encoding="utf-8") as file:
+        json.dump({ "inputs": inputs, "outputs": outputs}, file, indent=2)
