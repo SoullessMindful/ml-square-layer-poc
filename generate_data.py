54b191430238d61c07b354b6f09aa105a2a615cc
SoullessMindful <soullessmindful@gmail.com>
2025-07-30 23:48:16 +0200

    A more sophisticated cli argument parsing for the data generation script added as well as validation set generation.

--- generate_data.py
+++ generate_data.py
@@ -1,3 +1,4 @@
+import argparse
 from functools import reduce
 import json
 import random
@@ -6,26 +7,71 @@ from typing import Final
 
 
 VARIABLE_COUNT: Final = 10
-INPUTS_COUNT: Final = 10000
+DEFAULT_INPUTS_COUNT: Final = 10000
+DEFAULT_VALIDATION_INPUTS_COUNT: Final = 2000
 DEFAULT_DATA_PATH: Final = "./data/data.json"
 
 
-def generate_data():
+def generate_data(
+    inputs_count: int, validation_inputs_count: int
+) -> tuple[list[list[float]], list[list[float]], list[list[float]], list[list[float]]]:
     inputs: Final = [
         [random.uniform(-2, 2) for _ in range(VARIABLE_COUNT)]
-        for _ in range(INPUTS_COUNT)
+        for _ in range(inputs_count + validation_inputs_count)
     ]
     outputs: Final = [
         [reduce(lambda product, element: product * element, input_row, 1.0)]
         for input_row in inputs
     ]
-    return inputs, outputs
+
+    return (
+        inputs[:inputs_count],
+        outputs[:inputs_count],
+        inputs[inputs_count:],
+        outputs[inputs_count:],
+    )
 
 
 if __name__ == "__main__":
-    inputs, outputs = generate_data()
+    parser = argparse.ArgumentParser(description="Train models on generated data.")
+    parser.add_argument(
+        "-s",
+        "--inputs_count",
+        type=int,
+        default=DEFAULT_INPUTS_COUNT,
+        help="Size of the training set",
+    )
+    parser.add_argument(
+        "-vs",
+        "--validation_inputs_count",
+        type=int,
+        default=DEFAULT_VALIDATION_INPUTS_COUNT,
+        help="Size of the validation set",
+    )
+    parser.add_argument(
+        "-d",
+        "--data_path",
+        type=str,
+        default=DEFAULT_DATA_PATH,
+        help="Path to data file",
+    )
+    args = parser.parse_args()
+    inputs_count: Final[int] = args.inputs_count
+    validation_inputs_count: Final[int] = args.validation_inputs_count
+    data_path: Final[str] = args.data_path
 
-    path: Final = './data/data.json' if (len(sys.argv) <= 1) else sys.argv[1]
+    print(f"Generating data at {data_path}")
+    print(f"Training set size: {inputs_count}")
+    print(f"Validation set size: {validation_inputs_count}")
 
-    with open(path, "w", encoding="utf-8") as file:
-        json.dump({ "inputs": inputs, "outputs": outputs}, file, indent=2)
+    inputs, outputs, validation_inputs, validation_outputs = generate_data(
+        inputs_count, validation_inputs_count
+    )
+
+    with open(data_path, "w", encoding="utf-8") as file:
+        json.dump({
+            "inputs": inputs,
+            "outputs": outputs,
+            "validation_inputs": validation_inputs,
+            "validation_outputs": validation_outputs,
+        }, file, indent=2)
