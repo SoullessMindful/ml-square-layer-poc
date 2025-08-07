14db2adabb8e3e088977bc73f6b42cafbc113ae3
SoullessMindful <soullessmindful@gmail.com>
2025-08-07 22:11:16 +0200

    A proposed square model architecture for 3 variables.

--- models/square_model.py
+++ models/square_model.py
@@ -3,34 +3,25 @@ import torch.nn as nn
 from layers import SquareLayer
 
 class SquareModel(nn.Module):
-
+    
     def __init__(self, variable_count: int):
         super(SquareModel, self).__init__()
-
-        self.linear1 = nn.Linear(variable_count, 64)
-        self.activation1 = nn.SiLU()
-        self.square_layer1 = SquareLayer()
-        self.linear2 = nn.Linear(128, 128)
-        self.activation2 = nn.SiLU()
-        self.linear3 = nn.Linear(128, 32)
-        self.activation3 = nn.SiLU()
-        self.square_layer2 = SquareLayer()
-        self.linear4 = nn.Linear(64, 16)
-        self.activation4 = nn.SiLU()
-        self.square_layer3 = SquareLayer()
-        self.linear5 = nn.Linear(32, 1)
-
+        
+        self.activation = nn.Tanh
+        
+        self.model = nn.Sequential(
+            nn.Linear(variable_count, 32),
+            self.activation(),
+            SquareLayer(),
+            nn.Linear(64, 32),
+            self.activation(),
+            nn.Linear(32, 32),
+            self.activation(),
+            SquareLayer(),
+            nn.Linear(64, 32),
+            self.activation(),
+            nn.Linear(32, 1),
+        )
+    
     def forward(self, x):
-        x = self.linear1(x)
-        x = self.activation1(x)
-        x = self.square_layer1(x)
-        x = self.linear2(x)
-        x = self.activation2(x)
-        x = self.linear3(x)
-        x = self.activation3(x)
-        x = self.square_layer2(x)
-        x = self.linear4(x)
-        x = self.activation4(x)
-        x = self.square_layer3(x)
-        x = self.linear5(x)
-        return x
+        return self.model(x)
